6f8c1d617a924dcf63e0d8b9c39c1da32080cbb9
Yuri Henrique <yhls2004@gmail.com>
2025-04-18 14:05:24 -0300

    resolvido auto registro

--- terraform/realm.tf
+++ terraform/realm.tf
@@ -1,10 +1,10 @@
 //criando um resource do tipo keycloak realm chamando de autentica realm
 resource "keycloak_realm" "autentica_realm" {
   //nome do realm que irá aparecer na interface do keycloak
-  realm   = "autentica-realm"
+  realm   = "autentica_realm"
   //ativar o realm quando for criado
   enabled = true
-
+  
   //Ativar o botão de "Registrar-se"
   registration_allowed = true  
   //Permitir login com email
@@ -12,7 +12,12 @@ resource "keycloak_realm" "autentica_realm" {
   //Impedir e-mails duplicados
   duplicate_emails_allowed = false 
   //Permitir recuperação de senha
-  reset_password_allowed = true  
+  reset_password_allowed = true 
+  remember_me = true 
+  verify_email = false
   //Permitir editar o nome de usuário
-  edit_username_allowed = true  
+  edit_username_allowed = true 
+  login_theme = "keycloak" 
 }
+
+
