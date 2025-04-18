e9b42aac04c089c5da2dbf56afa8dcfa75be786e
Yuri Henrique <yhls2004@gmail.com>
2025-04-18 13:29:52 -0300

    tentando consertar o client account

--- terraform/realm.tf
+++ terraform/realm.tf
@@ -1,7 +1,7 @@
 //criando um resource do tipo keycloak realm chamando de autentica realm
 resource "keycloak_realm" "autentica_realm" {
   //nome do realm que irá aparecer na interface do keycloak
-  realm   = "projeto-realm"
+  realm   = "autentica-realm"
   //ativar o realm quando for criado
   enabled = true
 
