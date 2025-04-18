2acbd9cc4b311a02210df17fc520581bedc707cd
Yuri Henrique <yhls2004@gmail.com>
2025-04-18 12:42:03 -0300

    configurando o auto registro de usuario a (resolver ainda)

--- terraform/realm.tf
+++ terraform/realm.tf
@@ -4,4 +4,15 @@ resource "keycloak_realm" "autentica_realm" {
   realm   = "projeto-realm"
   //ativar o realm quando for criado
   enabled = true
+
+  //Ativar o botão de "Registrar-se"
+  registration_allowed = true  
+  //Permitir login com email
+  login_with_email_allowed = true  
+  //Impedir e-mails duplicados
+  duplicate_emails_allowed = false 
+  //Permitir recuperação de senha
+  reset_password_allowed = true  
+  //Permitir editar o nome de usuário
+  edit_username_allowed = true  
 }
